616a29aeee92150366269c6fa118cb7bef2e9ca9
Orestis zambounis <orestis.zambounis@gmail.com>
2019-09-17 13:53:17 +0700

    parameterize gunicorn

--- flask-gunicorn-nginx/attributes/default.rb
+++ flask-gunicorn-nginx/attributes/default.rb
@@ -1,2 +1,10 @@
+# general
 default['flask-gunicorn-nginx']['project_root'] = '/srv/www/'
 default['flask-gunicorn-nginx']['app_subfolder'] = ''
+
+# gunicorn
+
+default['flask-gunicorn-nginx']['gunicorn']["workers"] = 1
+default['flask-gunicorn-nginx']['gunicorn']["timeout"] = 30
+default['flask-gunicorn-nginx']['gunicorn']["log-level"] = "INFO"
+default['flask-gunicorn-nginx']['gunicorn']["worker-class"] = "sync"
